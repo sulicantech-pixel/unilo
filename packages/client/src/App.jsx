3948723dfa38eb0a9ac2acf996cac7d5ec522fa2
sulicantech-pixel <sulicantech@gmail.com>
2026-04-15 00:58:51 +0100

    Update App.jsx

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import BottomNav from './components/BottomNav';
-import HomePage from './pages/HomePage';
+import BottomNav from './components/BottomNav_Beautiful';
+import HomePage from './pages/HomePage_BeautifulFinal';
 import SearchPage from './pages/SearchPage';
 import ListingPage from './pages/ListingPage';
-import MapPage from './pages/MapPage';
-import CommunityPage from './pages/CommunityPage';
+import MapPage from './pages/MapPage_Searchable';
+import CommunityPage from './pages/CommunityPage_Beautiful';
 import WishlistPage from './pages/WishlistPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import BecomeHostPage from './pages/BecomeHostPage';
 import NotFoundPage from './pages/NotFoundPage';
-import QuickListModal from './components/QuickListModal';
+import QuickListModal from './components/QuickListModal_Beautiful';
 import { useAuthStore } from './store/authStore';
 
 export default function App() {
@@ -32,16 +33,17 @@ export default function App() {
   return (
     <div className="min-h-dvh flex flex-col">
       <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/"            element={<HomePage />} />
-          <Route path="/search"      element={<SearchPage />} />
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/listing/:id" element={<ListingPage />} />
-          <Route path="/map"         element={<MapPage />} />
-          <Route path="/community"   element={<CommunityPage />} />
-          <Route path="/wishlist"    element={<WishlistPage />} />
-          <Route path="/login"       element={<LoginPage />} />
-          <Route path="/register"    element={<RegisterPage />} />
-          <Route path="*"            element={<NotFoundPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/become-host" element={<BecomeHostPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </AnimatePresence>
 
