e94753b39bb03cda16f823531b141eab3e5c8ec8
sulicantech-pixel <sulicantech@gmail.com>
2026-05-17 01:46:50 +0100

    Update Wishlist.js

--- packages/server/src/models/Wishlist.js
+++ packages/server/src/models/Wishlist.js
@@ -1,8 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => sequelize.define('Wishlist', {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+  id:         { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
   user_id:    { type: DataTypes.UUID, allowNull: false },
   listing_id: { type: DataTypes.UUID, allowNull: false },
+  // Airbnb-style collections/folders
+  collection_name: { type: DataTypes.STRING, defaultValue: 'Saved rooms', allowNull: false },
+  note:            { type: DataTypes.TEXT,   allowNull: true  },
 }, {
   tableName: 'wishlists',
   indexes: [{ unique: true, fields: ['user_id', 'listing_id'] }],
